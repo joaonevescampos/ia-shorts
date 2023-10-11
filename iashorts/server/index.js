7eebf9ec017c7d3654ccb3babe8be32d86786ea4
João Campos <joaon.c.jv@gmail.com>
2023-10-11 20:18:02 -0300

    Add rota get

--- iashorts/server/index.js
+++ iashorts/server/index.js
@@ -11,6 +11,10 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  res.json({mensagem: 'Está funcionando!'})
+})
+
 
 app.get("/summary/:id", async (request, response) => {
   try {
